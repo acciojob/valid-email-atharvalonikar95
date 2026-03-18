ee20c8f700d4ce5fa6e3e440ff4f1a8e5486521f
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-03-18 17:58:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function validEmail(str) {
   //your JS code here.
-}
+	const regex = /^[a-z]+@[a-z]+\.[a-z]+$/
+	return regex.test(str)? true : false
+}   
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
